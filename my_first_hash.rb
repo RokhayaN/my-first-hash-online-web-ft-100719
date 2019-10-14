e4445538ba830fb7bab770c57d369a2928dbe081
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-14 17:01:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,12 +4,11 @@ my_hash = {"name" => "rokhaya", "gender"=> "female"}
 end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-def shipping_manifest = { "the_manifest"=> }
+def shipping_manifest = the_manifest
+  the_manifest ={"whale bone" => 5 , "porcelain vases" => 2 , "oil paintings"=> 3 }
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
 
 def retrieval
@@ -18,6 +17,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
