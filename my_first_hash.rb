93524dbe558c121f8deb745a68c99b85cb1f0051
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-14 17:06:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,7 @@ def retrieval
                  }
    shipping_manifest[2]              
                  
-
+end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
